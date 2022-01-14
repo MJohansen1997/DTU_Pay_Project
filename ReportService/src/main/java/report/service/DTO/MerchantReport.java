c804912abb571640bfd3b1edc2f2222e46d8267b
Janipai <s195477@student.dtu.dk>
2022-01-14 17:38:24 +0100

    added paymentID to the reportService

--- ReportService/src/main/java/report/service/DTO/MerchantReport.java
+++ ReportService/src/main/java/report/service/DTO/MerchantReport.java
@@ -6,8 +6,8 @@ import java.math.BigDecimal;
 public class MerchantReport extends Report implements Serializable {
     private static final long serialVersionUID = 9023222981284806610L;
 
-    public MerchantReport(String merchantID, String tokenID, String bankID, BigDecimal amount) {
-        super(merchantID, tokenID, bankID, amount);
+    public MerchantReport(String paymentID, String merchantID, String tokenID, String bankID, BigDecimal amount) {
+        super(paymentID, merchantID, tokenID, bankID, amount);
     }
     public MerchantReport(){super();}
 }

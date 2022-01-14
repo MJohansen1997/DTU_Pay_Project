b738e84cb1d307fcf1e030948604bdd1312aae84
Mjohansen1997 <mjohansen1997@gmail.com>
2022-01-14 12:46:37 +0100

    added some get's to avoid errors

--- DTUFacade/src/main/java/facades/customerFacade/CustomerResource.java
+++ DTUFacade/src/main/java/facades/customerFacade/CustomerResource.java
@@ -1,7 +1,15 @@
 package facades.customerFacade;
 
+import studentregistration.service.Student;
+
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 @Path("/customer")
 public class CustomerResource {
+    @GET
+    public String helloWorld() {
+        return "hello world";
+    }
 }
--- DTUFacade/src/main/java/facades/merchantFacade/MerchantResource.java
+++ DTUFacade/src/main/java/facades/merchantFacade/MerchantResource.java
@@ -1,7 +1,12 @@
 package facades.merchantFacade;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/merchant")
 public class MerchantResource {
+    @GET
+    public String helloWorld() {
+        return "hello world";
+    }
 }

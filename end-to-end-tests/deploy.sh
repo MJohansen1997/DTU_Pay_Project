910a039924b4a9200c7f283724fe6fa3a710b2f3
Mjohansen1997 <mjohansen1997@gmail.com>
2022-01-17 21:56:34 +0100

    lowered the sleep to 5

--- end-to-end-tests/deploy.sh
+++ end-to-end-tests/deploy.sh
@@ -2,6 +2,6 @@
 set -e
 docker image prune -f
 docker-compose up -d rabbitmq
-sleep 20
+sleep 5
 docker-compose up -d account-service-dtu payment-service-dtu report-service-dtu token-service-dtu dtu-facade-17
 sudo docker images

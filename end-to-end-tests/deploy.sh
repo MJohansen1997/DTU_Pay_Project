c7818bc504de15ade64630b1a4d1f6f87a14a486
Mjohansen1997 <mjohansen1997@gmail.com>
2022-01-17 21:56:07 +0100

    missed 1 renaming

--- end-to-end-tests/deploy.sh
+++ end-to-end-tests/deploy.sh
@@ -3,5 +3,5 @@ set -e
 docker image prune -f
 docker-compose up -d rabbitmq
 sleep 20
-docker-compose up -d account-service-dtu payment-service-dtu report-service-dtu token-service-dtu dtu-facade
+docker-compose up -d account-service-dtu payment-service-dtu report-service-dtu token-service-dtu dtu-facade-17
 sudo docker images

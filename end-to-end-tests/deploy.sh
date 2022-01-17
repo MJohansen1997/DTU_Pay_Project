33c94e3a5fa84e34a3a20bf948b15f29a662e64b
Mjohansen1997 <mjohansen1997@gmail.com>
2022-01-17 21:49:33 +0100

    tweaking around in the deploy.sh file

--- end-to-end-tests/deploy.sh
+++ end-to-end-tests/deploy.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
 docker image prune -f
-docker pull rabbitMq:latest
-docker-compose up -d rabbitMq
+docker-compose up -d rabbitmq
 sleep 20
 docker-compose up -d account-service-dtu payment-service-dtu report-service-dtu token-service-dtu dtu-facade
 sudo docker images

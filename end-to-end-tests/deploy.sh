b617287e45b3ecb16556332592ec2d83a2fd06c7
Mjohansen1997 <mjohansen1997@gmail.com>
2022-01-17 21:43:12 +0100

    made all repo names lowercase

--- end-to-end-tests/deploy.sh
+++ end-to-end-tests/deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 docker image prune -f
-docker pull rabbitMq:latest
-docker-compose up -d rabbitMq
+docker pull rabbitmq:latest
+docker-compose up -d rabbitmq
 sleep 20
 docker-compose up -d account-service-dtu payment-service-dtu report-service-dtu token-service-dtu dtu-facade
-
+sudo docker images

288a2381a2887b561654ddb57f6f019d1209efbf
Mjohansen1997 <mjohansen1997@gmail.com>
2022-01-17 22:25:55 +0100

    removed some containers that isn't implemented yet

--- end-to-end-tests/deploy.sh
+++ end-to-end-tests/deploy.sh
@@ -3,5 +3,4 @@ set -e
 docker image prune -f
 docker-compose up -d rabbitmq
 sleep 5
-docker-compose up -d account-service-dtu payment-service-dtu report-service-dtu token-service-dtu dtu-facade-17
-sudo docker images
+docker-compose up -d report-service-dtu token-service-dtu dtu-facade-17 #account-service-dtu payment-service-dtu

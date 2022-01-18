db83a5f30b2c22f9a2b10a30b15a5472504314fb
Mjohansen1997 <mjohansen1997@gmail.com>
2022-01-18 12:37:58 +0100

    changed all the hostnames to rabbitmq

--- ReportService/src/main/java/report/service/ReportStartUp.java
+++ ReportService/src/main/java/report/service/ReportStartUp.java
@@ -4,13 +4,14 @@ import messaging.implementations.RabbitMqQueue;
 import report.service.adapter.ReportController;
 
 
-public class StartUp {
+public class ReportStartUp {
     public static void main(String[] args) throws Exception {
-        new StartUp().startUp();
+        new ReportStartUp().startUp();
     }
 
     private void startUp() throws Exception {
         System.out.println("startup");
+//        var mq = new RabbitMqQueue("localhost");
         var mq = new RabbitMqQueue("rabbitmq");
         new ReportController(mq);
     }
